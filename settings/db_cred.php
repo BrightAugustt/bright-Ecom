10a5e20c3b1d4209ca6c158a0b2f40af553e9c20
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-10-23 20:40:20 +0100

    dd

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,8 +1,10 @@
 <?php
 //Database credentials
-Define('SERVER', 'localhost');
-Define('USERNAME', 'root');
-Define('PASSWD', '');
+Define('SERVER', 'us-cdbr-east-06.cleardb.net');
+Define('USERNAME', 'b8381e8687357a');
+Define('PASSWD', '246b0cb');
 Define('DATABASE', 'shoppn');
 
 ?>
+
+<!-- mysql://b8381e8687357a:246b0cb1@us-cdbr-east-06.cleardb.net/heroku_c41210addaccbb7?reconnect=true -->
